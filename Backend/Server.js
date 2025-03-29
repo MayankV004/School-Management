1bd5057521d4ecc6392c62449ed0fad0ba92d6fd
Mayank Verma <147617435+MayankV004@users.noreply.github.com>
2025-03-29 18:20:44 +0530

    Update Server.js

--- Backend/Server.js
+++ Backend/Server.js
@@ -17,7 +17,7 @@ if (process.env.INIT_DB === 'true') {
 app.use(express.json());
 app.use(express.urlencoded({extended:true}))
 app.use(cors({
-  origin: 'http://localhost:5173', // Your React app's address
+  origin: 'https://school-management-frontend-lwey.onrender.com', // Your React app's address
   methods: ['GET', 'POST', 'PUT', 'DELETE'],
   allowedHeaders: ['Content-Type', 'Authorization'],
 }));
